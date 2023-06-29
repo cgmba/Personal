32a55a1fc4a3f2d100ee638c9c888c7a52a54347
Mba Godwin <90321163+cgmba@users.noreply.github.com>
2023-06-29 22:05:11 +0100

    Update TestEightPuzzle.py

--- TestEightPuzzle.py
+++ TestEightPuzzle.py
@@ -10,7 +10,7 @@ goal_state = [['e', 1, 2], [3, 4, 5], [6, 7, 8]]
 class EightPuzzleTestClass(EightPuzzle):
     """
     This class is a wrapper for the EightPuzzle Class
-    It avoids specifying the goal state in every test..
+    It avoids specifying the goal state in every test.
     """
 
     def __init__(self, initial_state, goal=None):
